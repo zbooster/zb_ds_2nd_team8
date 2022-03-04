2fe63ee0b11ecdcb0c7eeadf9bf60cba0aae38f5
zpaladin1213 <zpaladin1213@gmail.com>
2022-03-04 20:58:44 +0900

    Changes

--- shkim/2_009/casting.py
+++ shkim/2_009/casting.py
@@ -13,3 +13,33 @@ print(type(var))
 var = bool(var)
 print(var)
 print(type(var))
+
+var1 = 'True'
+var2 = 'False'
+
+print(var1)
+print(var2)
+print(type(var1))
+print(type(var2))
+
+var1 = bool(var1)
+var2 = bool(var2)
+
+print(var1)
+print(var2)
+print(type(var1))
+print(type(var2))
+
+print(var1 + var2)
+
+var1 = 'True'
+var2 = 'False'
+
+var1 = bool(var1)
+var2 = bool(var2)
+
+print(var1 + var2)
+print(var1 - var2)
+print(var1 * var2)
+print(var1 / var2)
+
---
+++ shkim/2_011/dataio.py
@@ -0,0 +1,33 @@
+userName = 'hong gil dong'
+print(userName)
+
+userAge = 20
+print(userAge)
+
+print('User name:', userName)
+print('User age:', userAge)
+print('User name:', userName, 'User age:', userAge)
+
+print('3 * 6 = ', (3*6))
+
+print('3 * 5 = ')
+print((3*5))
+
+print('3 * 5 = ', end='')
+print((3*5))
+
+print(f'User name : {userName}')
+print(f'User age : {userAge}')
+
+print(f'User name : {userName}, User age : {userAge}')
+
+print(f'User name : {userName}\nUser age : {userAge}')
+print(f'User name :\t{userName}\nUser age :\t{userAge}')
+
+width = int(input('가로 길이 : '))
+height = int(input('세로 길이 : '))
+
+triangle = width * height / 2
+print(f'width : {width}, height : {height}, triangle : {triangle}')
+
+
